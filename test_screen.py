2e5ca564bab3c796b48ca59be049cae2756aeb6a
kenobijr <kenobijr@tatooine.com>
2025-03-03 18:43:55 +0100

    refactored test_screen.py

--- test_screen.py
+++ test_screen.py
@@ -1,10 +1,13 @@
 import pytest
 # from turtle import Screen
 from screen import GameScreen
+from unittest.mock import patch
 
 
-def test_GameScreen_init_attributes():
-    """test initiation instance params of screen object"""
+@patch("screen.Screen")
+@patch("screen.Turtle")
+def test_GameScreen_init_attributes(mock_screen_turtle_class, mock_screen_screen_class):
+    """test initiation instance params of screen object; patched out to prevent GUI since no turtle states needed"""
     screen = GameScreen()
     assert screen.width == 600
     assert screen.height == 600
@@ -14,18 +17,18 @@ def test_GameScreen_init_attributes():
     assert screen.left_boundary == -298
     assert screen.right_boundary == 296
 
-
-def test_GameScreen_init_screen_obj():
-    """test initiation of screen obj saved as class attribute"""
+@patch("screen.Turtle")
+def test_GameScreen_init_screen_obj(mock_screen_turtle_class):
+    """test initiation of screen obj saved as class attribute; patched out helper_turtle since not needed"""
     screen = GameScreen()
     assert hasattr(screen, "_turtle_screen")
     assert screen._turtle_screen.window_width() == 600
     assert screen._turtle_screen.window_height() == 600
     assert screen._turtle_screen.tracer() == 0
 
-
-def test_GameScreen_add_shapes():
-    """by initiating object "car.gif" and "explosion.gif" must be added as custom shaped"""
+@patch("screen.Turtle")
+def test_GameScreen_add_shapes(mock_screen_turtle_class):
+    """by initiating object "car.gif" and "explosion.gif" must be added as custom shape; patched out helper_turtle"""
     screen = GameScreen()
     registered_shapes = screen._turtle_screen.getshapes()
     assert "car.gif" in registered_shapes
